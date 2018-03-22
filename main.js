f5babc4835ec6f107fa37737ece60de888ea3487
yudai-uehara <yuudai_17n1100760@nnn.ed.jp>
2018-03-23 01:30:34 +0900

    delete loading message

--- main.js
+++ main.js
@@ -49,61 +49,62 @@ client.on("message", msg => {
 
       let r6s_player_find = msg.content.match(/^!r6s (.+)/)
       let roulette = msg.content.match(/^!roulette (.+)/)
-      let talk = msg.content.match(/^manager (.+)/)
 
       if (r6s_player_find) {
-        msg.channel.send(`Loading...`)
-        const name = r6s_player_find[1]
-
-        request.get(url.replace("<name>", name), (error, response, body) => {
-          if (error) {
-            msg.channel.send("GET request error");
-            return
-          }
-
-          const res = JSON.parse(body);
-
-          if (res.status === "failed") {
-            msg.channel.send(res.errors[0].title)
-            return
-          }
-
-          const casual = res.player.stats.casual
-          const rank = res.player.stats.ranked
-          const progression = res.player.stats.progression
-          const overall = res.player.stats.overall
-
-          //player
-          const player_data = settings.r6s_player_msg.replace("<level>", progression.level).replace("<xp>", progression.xp).replace("<headshots>", overall.headshots).replace("<assists>", overall.assists) + "\n\n"
-
-          //casual
-          const casual_data = settings.r6s_battle_record_msg.replace("<wins>", casual.wins).replace("<losses>", casual.losses).replace("<wlr>", casual.wlr).replace("<kills>", casual.kills).replace("<deaths>", casual.deaths).replace("<kd>", casual.kd).replace("<playtime>", Math.floor(casual.playtime / 3600)) + "\n\n"
-
-          //ranked
-          const rank_data = settings.r6s_battle_record_msg.replace("<wins>", rank.wins).replace("<losses>", rank.losses).replace("<wlr>", rank.wlr).replace("<kills>", rank.kills).replace("<deaths>", rank.deaths).replace("<kd>", rank.kd).replace("<playtime>", Math.floor(rank.playtime / 3600))
-
-          msg.channel.send({embed: {
-              color: 8421504,
-              title: `- ${name} -`,
-              fields: [{
-                  name: "\u200B",
-                  value: player_data
-                },{
-                  name: "\u200B",
-                  value: "\u200B"
-                },{
-                  name: "【カジュアル】",
-                  value: casual_data
-                },{
-                  name: "\u200B",
-                  value: "\u200B"
-                },{
-                  name: "【ランク】",
-                  value: rank_data
-                }
-              ],
+        msg.channel.send(settings.loading_msg).then(loading_msg => {
+          const name = r6s_player_find[1]
+          request.get(url.replace("<name>", name), (error, response, body) => {
+            if (error) {
+              msg.channel.send("GET request error");
+              return
             }
-          });
+
+            const res = JSON.parse(body);
+
+            if (res.status === "failed") {
+              msg.channel.send(res.errors[0].title)
+              return
+            }
+
+            const casual = res.player.stats.casual
+            const rank = res.player.stats.ranked
+            const progression = res.player.stats.progression
+            const overall = res.player.stats.overall
+
+            //player
+            const player_data = settings.r6s_player_msg.replace("<level>", progression.level).replace("<xp>", progression.xp).replace("<headshots>", overall.headshots).replace("<assists>", overall.assists) + "\n\n"
+
+            //casual
+            const casual_data = settings.r6s_battle_record_msg.replace("<wins>", casual.wins).replace("<losses>", casual.losses).replace("<wlr>", casual.wlr).replace("<kills>", casual.kills).replace("<deaths>", casual.deaths).replace("<kd>", casual.kd).replace("<playtime>", Math.floor(casual.playtime / 3600)) + "\n\n"
+
+            //ranked
+            const rank_data = settings.r6s_battle_record_msg.replace("<wins>", rank.wins).replace("<losses>", rank.losses).replace("<wlr>", rank.wlr).replace("<kills>", rank.kills).replace("<deaths>", rank.deaths).replace("<kd>", rank.kd).replace("<playtime>", Math.floor(rank.playtime / 3600))
+
+            msg.channel.send({embed: {
+                color: 8421504,
+                title: `- ${name} -`,
+                fields: [{
+                    name: "\u200B",
+                    value: player_data
+                  },{
+                    name: "\u200B",
+                    value: "\u200B"
+                  },{
+                    name: "【カジュアル】",
+                    value: casual_data
+                  },{
+                    name: "\u200B",
+                    value: "\u200B"
+                  },{
+                    name: "【ランク】",
+                    value: rank_data
+                  }
+                ],
+              }
+            }).then(res => {
+              loading_msg.delete()
+            });
+          })
         })
       }else if(roulette){
         const values = msg.content.replace("!roulette ","").split(" ")
