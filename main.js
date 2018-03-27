392f872e77d3c6a7726f810be8421617a04fcab2
yudai-uehara <yuudai_17n1100760@nnn.ed.jp>
2018-03-27 20:41:29 +0900

    add !r6s op command

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const request = require("request")
-const url = "https://api.r6stats.com/api/v1/players/<name>?platform=uplay"
+const player_api_url = "https://api.r6stats.com/api/v1/players/<name>?platform=uplay"
+const operator_api_url = "https://api.r6stats.com/api/v1/players/<name>/operators?platform=uplay"
 const discord = require("discord.js")
 const client = new discord.Client()
 const settings = require("./settings.json")[0]
@@ -65,13 +66,80 @@ client.on("message", msg => {
         }
       });
 
-      const r6s_player_find = msg.content.match(/^!r6s (.+)/)
-      const roulette = msg.content.match(/^!roulette (.+)/)
+      const r6s_player_data_find = msg.content.match(/^!r6s (.+)$/)
+      const r6s_operator_data_find = msg.content.match(/^!r6s op (.+)$/)
+      const roulette = msg.content.match(/^!roulette (.+)$/)
 
-      if (r6s_player_find) {
+      if(r6s_operator_data_find){
+         msg.channel.send(settings.loading_msg).then(loading_msg => {
+          const name = r6s_operator_data_find[1]
+          request.get(operator_api_url.replace("<name>", name), (error, response, body) => {
+            if (error) {
+              msg.channel.send("GET request error");
+              return
+            }
+
+            const res = JSON.parse(body);
+
+            if (res.status === "failed") {
+              msg.channel.send(res.errors[0].title)
+              return
+            }
+
+            const operators = res.operator_records
+            const indent = {name: "\u200b", value: "\u200b"}
+            let atack_operators = []
+            let defense_operators = []
+
+            //プレイ時間順　
+            const sorted = operators.sort((a, b) => {
+                const keyA = a.stats.played,
+                      keyB = b.stats.played;
+                if(keyA > keyB) return -1;
+                if(keyA < keyB) return 1;
+                return 0;
+            });
+
+            operators.some(op => {
+              const operator_data = settings.r6s_operator_msg.replace("<played>", op.stats.played).replace("<wlr>", Math.floor((op.stats.wins / op.stats.losses) * 1000) / 1000).replace("<kd>", Math.floor((op.stats.kills / op.stats.deaths) * 1000) / 1000).replace("<playtime>", Math.floor(op.stats.playtime / 3600))
+
+              if(op.operator.role === "atk" && atack_operators.length < settings.views_count * 2){
+                  atack_operators.push(indent)
+                  atack_operators.push({name: `- ${op.operator.name} -`, value: operator_data})
+              }else if(op.operator.role === "def" && defense_operators.length < settings.views_count * 2){
+                  defense_operators.push(indent)
+                  defense_operators.push({name: `- ${op.operator.name} -`, value: operator_data})
+              }else{
+                return false
+              }
+            })
+
+            msg.channel.send({embed: {
+                color: 8421504,
+                title: `【${name}】オペレーター情報 (プレイ時間順)`,
+            }}).then(sended_msg => {
+             return msg.channel.send({embed: {
+                color: 8421504,
+                title: "【攻撃】",
+                fields: atack_operators,
+              }
+            }).then(sended_msg => {
+              return msg.channel.send({embed: {
+                color: 8421504,
+                title: `【防衛】`,
+                fields: defense_operators,
+              }
+            }).then(sended_msg => {
+              loading_msg.delete()
+              })
+            })
+          })
+        })
+      })
+      } else if (r6s_player_data_find) {
         msg.channel.send(settings.loading_msg).then(loading_msg => {
-          const name = r6s_player_find[1]
-          request.get(url.replace("<name>", name), (error, response, body) => {
+          const name = r6s_player_data_find[1]
+          request.get(player_api_url.replace("<name>", name), (error, response, body) => {
             if (error) {
               msg.channel.send("GET request error");
               return
@@ -99,27 +167,26 @@ client.on("message", msg => {
             const rank_data = settings.r6s_battle_record_msg.replace("<wins>", rank.wins).replace("<losses>", rank.losses).replace("<wlr>", rank.wlr).replace("<kills>", rank.kills).replace("<deaths>", rank.deaths).replace("<kd>", rank.kd).replace("<playtime>", Math.floor(rank.playtime / 3600))
 
             msg.channel.send({embed: {
-                color: 8421504,
-                title: `- ${name} -`,
-                fields: [{
-                    name: "\u200B",
-                    value: player_data
-                  },{
-                    name: "\u200B",
-                    value: "\u200B"
-                  },{
-                    name: "【カジュアル】",
-                    value: casual_data
-                  },{
-                    name: "\u200B",
-                    value: "\u200B"
-                  },{
-                    name: "【ランク】",
-                    value: rank_data
-                  }
-                ],
-              }
-            }).then(res => {
+              color: 8421504,
+              title: `【${name}】戦績`,
+              fields: [{
+                  name: "\u200b",
+                  value: player_data
+                },{
+                  name: "\u200B",
+                  value: "\u200B"
+                },{
+                  name: "【カジュアル】",
+                  value: casual_data
+                },{
+                  name: "\u200B",
+                  value: "\u200B"
+                },{
+                  name: "【ランク】",
+                  value: rank_data
+                }
+              ]
+            }}).then(sended_msg => {
               loading_msg.delete()
             });
           })
